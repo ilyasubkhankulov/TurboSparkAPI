3b3e1881ab48f94896a237da0bf83fab22003dc6
Ilya Subkhankulov <ilya.subkhankulov@gmail.com>
2023-11-25 17:47:47 -0800

    formatted

--- main.py
+++ main.py
@@ -1,55 +1,59 @@
-from fastapi import FastAPI, File, UploadFile, Form
-from fastapi.responses import JSONResponse
 import pandas as pd
+from fastapi import FastAPI, File, Form, UploadFile
+from fastapi.responses import JSONResponse
+
 # Import additional necessary libraries here
 
 app = FastAPI()
 
 
 @app.post("/upload")
-async def upload_files(csv_file: UploadFile = File(...),
-                       chart_image: UploadFile = File(...),
-                       prompt: str = Form(...)):
-  """
+async def upload_files(
+    csv_file: UploadFile = File(...),
+    chart_image: UploadFile = File(...),
+    prompt: str = Form(...),
+):
+    """
     Endpoint to upload CSV files, an image of a chart, and a user prompt.
     """
-  # Process the uploaded CSV file
-  df = pd.read_csv(csv_file.file)
-  # Add logic to process the CSV file according to the user's prompt
+    # Process the uploaded CSV file
+    df = pd.read_csv(csv_file.file)
+    # Add logic to process the CSV file according to the user's prompt
 
-  # Process the uploaded chart image
-  # Add logic to process the image file if necessary
+    # Process the uploaded chart image
+    # Add logic to process the image file if necessary
 
-  # Return a response indicating success or failure
-  return {"message": "Files received and being processed"}
+    # Return a response indicating success or failure
+    return {"message": "Files received and being processed"}
 
 
 @app.post("/feedback")
 async def receive_feedback(feedback: str):
-  """
+    """
     Endpoint to receive user feedback for further refining the data processing.
     """
-  # Add logic to process feedback
-  # Adjust data processing as needed
+    # Add logic to process feedback
+    # Adjust data processing as needed
 
-  # Return updated results or confirmation
-  return {"message": "Feedback received and processed"}
+    # Return updated results or confirmation
+    return {"message": "Feedback received and processed"}
 
 
 @app.get("/visualization")
 async def get_visualization():
-  """
+    """
     Endpoint to get the processed data visualization.
     """
-  # Logic to return the data visualization
-  # This could be in the form of a URL to an image, a JSON structure, etc.
+    # Logic to return the data visualization
+    # This could be in the form of a URL to an image, a JSON structure, etc.
 
-  # Placeholder response
-  return {"message": "Visualization not implemented yet"}
+    # Placeholder response
+    return {"message": "Visualization not implemented yet"}
 
 
 # Additional routes and logic can be added as needed
 
 if __name__ == "__main__":
-  import uvicorn
-  uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
